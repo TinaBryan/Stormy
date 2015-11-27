985576d62b69be0a03744ef9518b28f885d312b2
Tina Bryan <tinabryan@Tinas-MBC.local>
2015-11-27 00:37:33 -0500

    first commit

---
+++ AndroidStudioProjects/GolfScorecard/app/src/main/java/com/codelouisville/tinabryan/golfscorecard/Hole.java
@@ -0,0 +1,7 @@
+package com.codelouisville.tinabryan.golfscorecard;
+
+/**
+ * Created by tinabryan on 11/26/15.
+ */
+public class Hole {
+}
---
+++ AndroidStudioProjects/Stormy/app/src/main/java/teamtreehouse/com/stormy/adapters/HourAdapter.java
@@ -0,0 +1,7 @@
+package teamtreehouse.com.stormy.adapters;
+
+/**
+ * Created by tinabryan on 11/25/15.
+ */
+public class HourAdapter {
+}
